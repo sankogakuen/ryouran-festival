9a5ba4683c8f8f9ad0f1f4bbb1db212653405f15
dangornushi <dangomushi150@gmail.com>
2023-10-04 18:02:35 +0900

    ADD: Youtubeを埋め込み

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import FadeInImage from '../../components/FadingImage'
-import Access from './Access'
 import Top_title_img from '../../static/img/Top-title-img.png'
 import ScrollUp from '../../utils/ScrollUp'
-import Page from '../../components/Page';
+import FadeInImage from '../../components/FadingImage'
+
+import Access from './Access'
+import ThemeSong from './ThemeSong'
 
 const Home: React.FC = () => {
     return (
-        <>
-            <FadeInImage src={Top_title_img} alt="sanko festival" />
-            <Page>
-                <Access />
-            </Page>
-            <ScrollUp />
-        </>
+    <>
+      <FadeInImage src={Top_title_img} alt="sanko festival" />
+      <Access />
+      <ThemeSong/>
+      <ScrollUp /> 
+    </>
     );
 };
 
