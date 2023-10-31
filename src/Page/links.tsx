4c0ec126369e1068d1fa09b155ee1b220953f24d
dangornushi <dangomushi150@gmail.com>
2023-10-31 13:47:48 +0900

    FIX

--- src/Page/links.tsx
+++ src/Page/links.tsx
@@ -7,6 +7,7 @@ import Page from "../Components/Page";
 import InstagramImg from "../static/img/InstagramImg.png";
 import TwitterImg from "../static/img/TwitterImg.png";
 import OmiyaHpImg from "../static/img/omiyaHP.png";
+import SlideInFadeInComponent from "../Components/SlideInFadeInComponent";
 
 import {
   AiOutlineGlobal,
@@ -20,35 +21,37 @@ const links: React.FC = () => {
       <Box height={"90vh"} width={"80vw"}>
         <NewH1>Links</NewH1>
 
-        <Grid container spacing={2}>
-          <Grid item xs={12} sm={4}>
-            <BodyCard
-              title="学校公式HP"
-              avatar={<AiOutlineGlobal />}
-              imageUrl={OmiyaHpImg}
-              text="飛鳥未来きずな高校大宮CP公式HP"
-              popUpUrl="https://www.sanko.ac.jp/asuka-kizuna/omiya/"
-            />
+        <SlideInFadeInComponent>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={4}>
+              <BodyCard
+                title="学校公式HP"
+                avatar={<AiOutlineGlobal />}
+                imageUrl={OmiyaHpImg}
+                text="飛鳥未来きずな高校大宮CP公式HP"
+                popUpUrl="https://www.sanko.ac.jp/asuka-kizuna/omiya/"
+              />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <BodyCard
+                title="Instagram"
+                avatar={<AiOutlineInstagram />}
+                imageUrl={InstagramImg}
+                text="大宮CP公式インスタグラム"
+                popUpUrl="https://www.instagram.com/omiya.asukamiraikizuna/"
+              />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <BodyCard
+                title="Twitter / X"
+                avatar={<AiFillTwitterSquare />}
+                imageUrl={TwitterImg}
+                text="大宮CP公式Twitter(X)"
+                popUpUrl="https://twitter.com/kizunaomiya"
+              />
+            </Grid>
           </Grid>
-          <Grid item xs={12} sm={4}>
-            <BodyCard
-              title="Instagram"
-              avatar={<AiOutlineInstagram />}
-              imageUrl={InstagramImg}
-              text="大宮CP公式インスタグラム"
-              popUpUrl="https://www.instagram.com/omiya.asukamiraikizuna/"
-            />
-          </Grid>
-          <Grid item xs={12} sm={4}>
-            <BodyCard
-              title="Twitter / X"
-              avatar={<AiFillTwitterSquare />}
-              imageUrl={TwitterImg}
-              text="大宮CP公式Twitter(X)"
-              popUpUrl="https://twitter.com/kizunaomiya"
-            />
-          </Grid>
-        </Grid>
+        </SlideInFadeInComponent>
       </Box>
     </Page>
   );
--- src/Page/themesong.tsx
+++ src/Page/themesong.tsx
@@ -2,20 +2,23 @@ import * as React from "react";
 import { Box, Grid } from "@mui/material";
 import Page from "../Components/Page";
 import NewH1 from "../Components/NewH1";
+import SlideInFadeInComponent from "../Components/SlideInFadeInComponent";
 
 const themesong: React.FC = () => {
   return (
     <Page sectionId="Theme song">
       <NewH1>Theme song</NewH1>
-      <Box width={"80vw"} height={"50vh"}>
-        <iframe
-          width="100%"
-          height="100%"
-          src="https://www.youtube.com/embed/ulforNn47PM"
-          title="緑黄色社会『キャラクター』Official Video / Ryokuoushoku Shakai – Character"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        ></iframe>
-      </Box>
+      <SlideInFadeInComponent>
+        <Box width={"80vw"} height={"50vh"}>
+          <iframe
+            width="100%"
+            height="100%"
+            src="https://www.youtube.com/embed/ulforNn47PM"
+            title="緑黄色社会『キャラクター』Official Video / Ryokuoushoku Shakai – Character"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          ></iframe>
+        </Box>
+      </SlideInFadeInComponent>
     </Page>
   );
 };
